172131ef17cc30b8f6bcd4fa553e2b25e41b8b32
dapod93 <davidliois50@gmail.com>
2025-03-16 16:10:20 +0700

    update: index
    Signed-off-by: dapod93 <davidliois50@gmail.com>

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/api/:date?", function (req, res) {
   }
 
   if (isNaN(date.getTime())) {
-    return res.json({ error: "Invalid Date" });
+    res.json({ error: "Invalid Date" });
   }
 
   res.json({ unix: date.getTime(), utc: date.toUTCString() });
